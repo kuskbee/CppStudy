f2f170f073b8105be8b25cac1a51e7c1be5dc359
kusk <lpunch3corns@naver.com>
2025-04-08 15:00:02 +0900

    feat : add Resizing Array Function

--- L20250408_DynamicAlloc/main.cpp
+++ L20250408_DynamicAlloc/main.cpp
@@ -2,31 +2,50 @@
 
 using namespace std;
 
+int ResizeArray(int** DynamicArray, int ArraySize);
+
 int main()
 {
-	int* DynamicArray = new int[10];
+	int ArraySize = 10;
+	int* DynamicArray = new int[ArraySize];
 
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < ArraySize; i++)
 	{
 		DynamicArray[i] = (i + 1) * 3;
 	}
 
-	int* NewDynamicArray = new int[20];
-	memcpy(NewDynamicArray, DynamicArray, sizeof(int) * 10);
-
-	int* Temp = DynamicArray;
-	for (int j = 10; j < 20; j++)
-	{
-		NewDynamicArray[j] = (j + 1) * 4;
-	}
-	DynamicArray = NewDynamicArray;
-	delete Temp;
-	Temp = nullptr;
+	ArraySize = ResizeArray(&DynamicArray, ArraySize);
 
 	// DynamicArray[20] 로 할당, -> 10-19 idx는 -> 4배 넣기
+	for (int i = 10; i < ArraySize; i++)
+	{
+		DynamicArray[i] = (i + 1) * 4;
+	}
 
-	for (int i = 0; i < 20; i++)
+	ArraySize = ResizeArray(&DynamicArray, ArraySize);
+
+	// DynamicArray[20] 로 할당, -> 20-39 idx는 -> 5배 넣기
+	for (int i = 20; i < ArraySize; i++)
+	{
+		DynamicArray[i] = (i + 1) * 5;
+	}
+
+	for (int i = 0; i < ArraySize; i++)
 	{
 		cout << DynamicArray[i] << endl;
 	}
+}
+
+int ResizeArray(int** DynamicArray, int ArraySize)
+{
+	int NewArraySize = ArraySize * 2;
+	int* NewDynamicArray = new int[NewArraySize];
+	memcpy(NewDynamicArray, *DynamicArray, sizeof(int) * ArraySize);
+
+	int* Temp = *DynamicArray;
+	*DynamicArray = NewDynamicArray;
+	delete[] Temp;
+	Temp = nullptr;
+
+	return NewArraySize;
 }
